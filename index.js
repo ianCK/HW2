41e9c2d508ac49583bad5e1a79ac84b7ffb1a706
ianCK <b07102015@ntu.edu.tw>
2019-06-01 12:56:31 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,123 @@
+var cnt = 0,
+    now = 0,
+    total
+
+$(document).ready(function() {
+    $('#decoline').removeClass('hide')
+    $('#side_nav_btn').removeClass('hide')
+    $('#next_page_btn').removeClass('hide')
+    $('#decoline').addClass('start')
+    $('#side_nav_btn').addClass('start')
+    $('#next_page_btn').addClass('start')
+    $('#side_nav_btn').on('animationend', function() {
+        $(this).removeClass('start')
+        $(this).off('animationend')
+        cnt++
+        if (cnt == 3) start()
+    })
+    $('#next_page_btn').on('animationend', function() {
+        $(this).removeClass('start')
+        $(this).off('animationend')
+        cnt++
+        if (cnt == 3) start()
+    })
+    $('#decoline').on('animationend', function() {
+        $(this).removeClass('start')
+        $(this).off('animationend')
+        cnt++
+        if (cnt == 3) start()
+    })
+})
+
+function newItem(item, id) {
+    var img = $('<img>').attr('src', item.image).attr('class', 'image'),
+        name = $('<h3>').attr('class', 'name').text(item.name),
+        price = $('<p>').attr('class', 'price').text('NT$ ' + item.price),
+        div = $('<div>').attr('class', 'item show').attr('id', 'item' + id)
+    $(div).append(img).append(name).append(price)
+    if (id % 20 == 0) $('#items').append($('<div>').attr('class', 'page hide').attr('id', 'page' + Math.floor(id / 20)))
+    $('#page' + Math.floor(id / 20)).append(div)
+}
+
+
+function next_page() {
+    var last = $('#page' + now),
+        nxt = $('#page' + (now + 1))
+    $(last).attr('class', 'page')
+    $(last).attr('class', 'page left_out')
+    $(nxt).attr('class', 'page')
+    $(nxt).attr('class', 'page left_in')
+}
+
+function prev_page() {
+    var last = $('#page' + now),
+        nxt = $('#page' + (now - 1))
+    $(last).attr('class', 'page')
+    $(last).attr('class', 'page right_out')
+    $(nxt).attr('class', 'page')
+    $(nxt).attr('class', 'page right_in')
+}
+
+function start() {
+    $('#contentwrap').removeClass('hide')
+    $('#title').addClass('start')
+    $('#title').on('animationend', function() {
+        $(this).removeClass('start')
+        $(this).off('animationend')
+        $('#side_nav_btn').on('click', function(e) {
+            if (e.which == 1) {
+                if ($(this).hasClass('active')) {
+                    $(this).removeClass('active')
+                    $(this).addClass('deactive')
+                    $('#contentwrap').addClass('close')
+                    $('#contentwrap').removeClass('open')
+                    $('#side_nav_menu').addClass('close')
+                    $('#side_nav_menu').removeClass('open')
+                    $('#next_page_btn').addClass('close')
+                    $('#next_page_btn').removeClass('open')
+                    $('#prev_page_btn').addClass('close')
+                    $('#prev_page_btn').removeClass('open')
+
+                } else {
+                    $(this).removeClass('deactive')
+                    $(this).addClass('active')
+                    $('#contentwrap').addClass('open')
+                    $('#contentwrap').removeClass('close')
+                    $('#side_nav_menu').addClass('open')
+                    $('#side_nav_menu').removeClass('close')
+                    $('#next_page_btn').addClass('open')
+                    $('#next_page_btn').removeClass('close')
+                    $('#prev_page_btn').addClass('open')
+                    $('#prev_page_btn').removeClass('close')
+                }
+            }
+        })
+        $('#next_page_btn').on('click', function(e) {
+            if (e.which == 1) {
+                next_page()
+                now++
+                if (now != 0) $('#prev_page_btn').removeClass('hide')
+                if (now == Math.floor(total / 20)) $('#next_page_btn').addClass('hide')
+            }
+        })
+        $('#prev_page_btn').on('click', function(e) {
+            if (e.which == 1) {
+                prev_page()
+                now--
+                if (now == 0) $('#prev_page_btn').addClass('hide')
+                if (now != Math.floor(total / 20)) $('#next_page_btn').removeClass('hide')
+            }
+        })
+        $.get('https://js.kchen.club/B07102015/query', function(response) {
+            if (response) {
+                if (response.result) {
+                    items = response.items
+                    total = items.length
+                    for (var i = 0; i < total; i++) newItem(items[i], i)
+                    $('#page0').attr('class', 'page')
+                }
+            }
+            $('#footer').removeClass('hide')
+        }, "json")
+    })
+}
---
+++ insert.js
@@ -0,0 +1,108 @@
+var cnt = 0
+
+$(document).ready(function() {
+    $('#decoline').removeClass('hide')
+    $('#side_nav_btn').removeClass('hide')
+    $('#decoline').addClass('start')
+    $('#side_nav_btn').addClass('start')
+    $('#side_nav_btn').on('animationend', function() {
+        $(this).removeClass('start')
+        $(this).off('animationend')
+        cnt++
+        if (cnt == 2) start()
+    })
+    $('#decoline').on('animationend', function() {
+        $(this).removeClass('start')
+        $(this).off('animationend')
+        cnt++
+        if (cnt == 2) start()
+    })
+})
+
+function warning(s) {
+    $('#warning').text(s)
+}
+
+function check_product(item) {
+    $('#item').remove()
+    var img = $('<img>').attr('src', item.image).attr('class', 'image'),
+        name = $('<h3>').attr('class', 'name').text(item.name),
+        price = $('<p>').attr('class', 'price').text('NT$ ' + item.price),
+        div = $('<div>').attr('class', 'item').attr('id', 'item')
+    $(div).append(img).append(name).append(price)
+    $('#show_area').append(div)
+    if (!$('#show_area').hasClass('in')) $('#show_area').attr('class', 'show_area in')
+}
+
+function clear_product() {
+    $('#item').remove()
+    $('#show_area').attr('class', 'show_area out')
+    $('#show_area').on('animationend', function() {
+        $(this).addClass('hide')
+        $(this).off('animationend')
+    })
+}
+
+function start() {
+    $('#contentwrap').removeClass('hide')
+    $('#input_area').addClass('start')
+    $('#input_area').on('animationend', function() {
+        $('#input_area').removeClass('start')
+        $('#side_nav_btn').on('click', function(e) {
+            if (e.which == 1) {
+                if ($(this).hasClass('active')) {
+                    $(this).removeClass('active')
+                    $(this).addClass('deactive')
+                    $('#contentwrap').addClass('close')
+                    $('#contentwrap').removeClass('open')
+                    $('#side_nav_menu').addClass('close')
+                    $('#side_nav_menu').removeClass('open')
+                } else {
+                    $(this).removeClass('deactive')
+                    $(this).addClass('active')
+                    $('#contentwrap').addClass('open')
+                    $('#contentwrap').removeClass('close')
+                    $('#side_nav_menu').addClass('open')
+                    $('#side_nav_menu').removeClass('close')
+                }
+            }
+        })
+        $('#insert').on('click', function(e) {
+            if (e.which == 1) {
+                var data = {
+                    item: {
+                        name: $('#name').val(),
+                        price: $('#price').val(),
+                        count: $('#count').val(),
+                        image: $('#image').val()
+                    }
+                }
+                if (data.item.name == "") warning("請輸入商品名稱!!")
+                else if (data.item.price == "") warning("請輸入商品價格!!")
+                else if (isNaN(Number(data.item.price)) || Number(data.item.price) <= 0) warning("商品價格須為正整數!!")
+                else if (data.item.count == "") warning("請輸入商品數量!!")
+                else if (isNaN(Number(data.item.count)) || Number(data.item.count) <= 0) warning("商品數量須為正整數!!")
+                else if (data.item.image == "") warning("請輸入商品圖片網址!!")
+                else {
+                    data.item.price = Number(data.item.price)
+                    data.item.count = Number(data.item.count)
+                    check_product(data.item)
+                    warning("")
+                    $('#check').on('click', function(e) {
+                        if (e.which == 1) {
+                            $.post('https://js.kchen.club/B12345678/insert', data, function(response) {
+                                if (response) {
+                                    if (response.result) {
+                                        $(this).off('click')
+                                        clear_product()
+                                        warning("商品新增成功")
+                                    } else warning("新增失敗")
+                                } else warning("伺服器出錯")
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    })
+}
